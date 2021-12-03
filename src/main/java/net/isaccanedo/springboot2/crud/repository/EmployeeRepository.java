e0e549c5a0ec8bcd91021ee35dc1ac788ad72201
isaccanedo <isaccanedo@gmail.com>
2021-12-03 13:56:28 -0300

    Initial commit

---
+++ src/main/java/net/isaccanedo/springboot2/crud/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package net.isaccanedo.springboot2.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.isaccanedo.springboot2.crud.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
